7a2e5166a04848e938a20012c4bacc16ca149843
luohuidong <luohuidong01@126.com>
2021-10-03 21:31:35 +0800

    build: 配置路径别名与添加 JSX 语法支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "src"),
+    },
+  },
+});
