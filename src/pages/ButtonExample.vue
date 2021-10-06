58de1abadaae85522819558c048b0a15be7513d8
luohuidong <luohuidong01@126.com>
2021-10-06 14:13:10 +0800

    feat: 添加按钮例子

---
+++ src/pages/ButtonExample.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Button from "@/components/Button";
+</script>
+
+<template>
+  <Button text="text button"></Button>
+  <Button type="outlined" text="outlined button"></Button>
+  <Button type="contained" text="contained button"></Button>
+</template>
