7d95c143884dbe436ed8b79442c849f4af2dd19a
luohuidong <luohuidong01@126.com>
2021-12-27 01:08:45 +0800

    refactor: 重构 scrim 组件

---
+++ src/components/Scrim.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const emits = defineEmits(["click"]);
+
+function handleClick(event: MouseEvent) {
+  emits("click", event);
+}
+</script>
+
+<template>
+  <div :class="$style.scrim" @click.self="handleClick">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" module>
+@use "@/styles/color";
+
+.scrim {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  left: 0px;
+  bottom: 0px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+
+  background-color: rgba(color.$background-dark, 0.7);
+}
+</style>
