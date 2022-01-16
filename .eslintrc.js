7f65eeabf1e6b511fe0f156c412dce38c746c6d7
luohuidong <luohuidong01@126.com>
2022-01-17 00:24:16 +0800

    refactor: 配置 eslint ts 校验规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
   extends: ["plugin:vue/vue3-recommended", "prettier"],
   rules: {},
 };
