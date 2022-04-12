a097d7fe33decc7326d653b049ff2c44dcc6a7a2
luohuidong <luohuidong01@126.com>
2022-04-12 14:05:50 +0800

    feat: table 新增 slot 的方式渲染内容

---
+++ packages/components/src/examples/Table/CaseSlotTable.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { h } from "vue";
+import { VmTable } from "@/components/Table";
+import data from "./data.ts";
+
+type Data = InstanceType<typeof VmTable>["$props"]["data"];
+
+const columns: InstanceType<typeof VmTable>["$props"]["columns"] = [
+  {
+    title: "姓名",
+    key: "name",
+    slotName: "name",
+  },
+  {
+    title: "年龄",
+    key: "age",
+    slotName: "age",
+  },
+  {
+    title: "出生日期",
+    key: "birthday",
+    slotName: "birthday",
+  },
+  {
+    title: "地址",
+    key: "address",
+    slotName: "address",
+  },
+];
+
+const rowExpand: InstanceType<typeof VmTable>["$props"]["rowExpand"] = {
+  expandCondition: (row: Data[number]) =>
+    (row.files && [row.files as Array<number>].length) as boolean,
+  expandSlotName: "rowExpand",
+};
+</script>
+
+<template>
+  <VmTable
+    unique-key="id"
+    :columns="columns"
+    :data="data"
+    :row-expand="rowExpand"
+  >
+    <template #name="{ rowData }">
+      {{ rowData.name }}
+    </template>
+
+    <template #age="{ rowData }">
+      {{ rowData.age + "岁" }}
+    </template>
+
+    <template #birthday="{ rowData }">
+      {{ rowData.birthday }}
+    </template>
+
+    <template #address="{ rowData }">
+      {{ rowData.address }}
+    </template>
+
+    <template #rowExpand="{ rowData }">
+      {{ `${rowData.name}: ${rowData.age} 岁` }}
+    </template>
+  </VmTable>
+</template>
+
+<style lang="less" scoped></style>
---
+++ packages/components/src/examples/Table/Table.vue
@@ -0,0 +1,24 @@
+<route lang="yaml">
+name: Table
+meta:
+  title: 表格 (Table)
+</route>
+
+<script setup lang="ts">
+import CaseVNodeTable from "./CaseVNodeTable.vue";
+import CaseSlotTable from "./CaseSlotTable.vue";
+</script>
+
+<template>
+  <section>
+    <h1>使用 VNode 方式的表格</h1>
+    <CaseVNodeTable></CaseVNodeTable>
+  </section>
+
+  <section>
+    <h1>使用 slot 方式的表格</h1>
+    <CaseSlotTable></CaseSlotTable>
+  </section>
+</template>
+
+<style scoped></style>
