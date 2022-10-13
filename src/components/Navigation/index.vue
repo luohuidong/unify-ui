271b720a3836c243b97f58867cfcb9c80027bc9f
luohuidong <luohuidong01@126.com>
2022-10-13 22:00:36 +0800

    refactor: 更改目录结构

---
+++ src/components/Navigation/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "VMNavigation",
+});
+</script>
+
+<script lang="ts" setup></script>
+
+<template>
+  <div></div>
+</template>
+
+<style lang="scss" module></style>
