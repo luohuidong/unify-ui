d57644160fa53ebf73fe5862ded3e7833e6131c7
luohuidong <luohuidong01@126.com>
2023-02-06 17:28:52 +0800

    docs: new button docs

---
+++ src/lib/components/Button/docs/index.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from "@/lib/components";
+</script>
+
+<template>
+  <Story title="Button">
+    <Button type="text" text="text button"></Button>
+    <Button type="outlined" text="outlined button"></Button>
+    <Button type="contained" text="contained button"></Button>
+  </Story>
+</template>
