092dc303bb79b89f7645deca75244fc7075cc20c
luohuidong <luohuidong01@126.com>
2023-02-06 20:30:59 +0800

    feat: add checkbox

---
+++ src/lib/components/Checkbox/docs/index.story.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import SCheckbox from "../index.vue";
+</script>
+
+<template>
+  <Story title="s-checkbox">
+    <div class="container">
+      <s-checkbox label="One"></s-checkbox>
+      <s-checkbox label="Two"></s-checkbox>
+      <s-checkbox label="Three"></s-checkbox>
+      <s-checkbox label="Four"></s-checkbox>
+    </div>
+  </Story>
+</template>
+
+<style scoped>
+.container {
+  height: 100px;
+}
+</style>
---
+++ src/lib/components/Checkbox/index.vue
@@ -0,0 +1,75 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SCheckbox",
+});
+</script>
+
+<script setup lang="ts">
+defineProps<{
+  label: string;
+}>();
+</script>
+
+<template>
+  <label :class="$style.container">
+    <input :class="$style.input" type="checkbox" checked />
+    <span :class="$style.checkmark"></span>
+    {{ label }}
+  </label>
+</template>
+
+<style lang="scss" module>
+.container {
+  height: 26px;
+  display: inline-flex;
+  align-items: center;
+  margin-right: 30px;
+  cursor: pointer;
+  font-size: 14px;
+  user-select: none;
+}
+
+.input {
+  display: none;
+}
+
+// custom checkbox
+.checkmark {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 14px;
+  width: 14px;
+  margin-right: 8px;
+  background-color: #eee;
+  border-radius: 2px;
+}
+
+// When the checkbox is checked, add a blue background
+.input:checked ~ .checkmark {
+  background-color: #037aff;
+}
+
+// On mouse-over, add a grey background color
+.container:hover > .checkmark {
+  background-color: #ccc;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.container > .checkmark::after {
+  display: none;
+  content: "";
+  width: 2px;
+  height: 6px;
+  border: solid white;
+  border-width: 0 2px 2px 0;
+  transform: rotate(45deg);
+}
+
+/* Show the checkmark when checked */
+.container > input:checked ~ .checkmark::after {
+  display: block;
+}
+</style>
