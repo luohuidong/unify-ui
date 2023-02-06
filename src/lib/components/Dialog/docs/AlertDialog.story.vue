d03adc46af3a818d46ccceb87dd1a7f86c5e04e3
luohuidong <luohuidong01@126.com>
2023-02-06 17:29:22 +0800

    docs: new dialog docs

---
+++ src/lib/components/Dialog/docs/AlertDialog.story.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useAlertDialog, Button } from "@/lib/components";
+
+const { confirm } = useAlertDialog();
+
+function handleConfirmDialogButton() {
+  confirm({
+    title: "提示",
+    supportingText: "是否确定删除该记录",
+    onConfirm: () => {},
+    onCancel: () => {},
+  });
+}
+</script>
+
+<template>
+  <Story title="Feedback/Dialog/AlertDialog">
+    <Button
+      type="contained"
+      text="confirm Dialog"
+      @click="handleConfirmDialogButton"
+    ></Button>
+  </Story>
+</template>
