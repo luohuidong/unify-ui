b2b1e9cabfc4c44f2dbd6590203bf04b31cfec1e
luohuidong <luohuidong01@126.com>
2023-02-07 18:12:42 +0800

    docs: create the docs hierarchy

--- src/lib/components/Button/docs/index.story.vue
+++ src/lib/components/Button/docs/index.story.vue
@@ -3,7 +3,7 @@ import { Button } from "@/lib/components";
 </script>
 
 <template>
-  <Story title="Button">
+  <Story title="Generel/Button">
     <Button type="text" text="text button"></Button>
     <Button type="outlined" text="outlined button"></Button>
     <Button type="contained" text="contained button"></Button>
--- src/lib/components/Checkbox/docs/index.story.vue
+++ src/lib/components/Checkbox/docs/index.story.vue
@@ -8,7 +8,7 @@ const checked2 = ref(false);
 </script>
 
 <template>
-  <Story title="s-checkbox">
+  <Story title="Data Entry/Checkbox">
     <div class="container">
       <s-checkbox v-model="checked1" label="One"></s-checkbox>
       <s-checkbox v-model="checked2" label="Two"></s-checkbox>
