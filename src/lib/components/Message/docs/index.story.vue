00bdc12e59166ca82fc6f607e3d26a0672d9cdfa
luohuidong <luohuidong01@126.com>
2023-02-08 09:18:58 +0800

    docs: add message docs

---
+++ src/lib/components/Message/docs/index.story.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Button, useMessage } from "@/lib/components";
+
+const { message } = useMessage();
+
+function handleClick() {
+  message({
+    text: "测试",
+  });
+}
+</script>
+
+<template>
+  <Story title="Message">
+    <Button type="contained" text="添加 message" @click="handleClick"></Button>
+  </Story>
+</template>
