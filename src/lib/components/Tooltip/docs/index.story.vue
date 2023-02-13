7e5cae3dcbb07e2da7ac0dac708d9eebc7c073ca
luohuidong <luohuidong01@126.com>
2023-02-13 21:08:46 +0800

    feat: add tooltip component

---
+++ src/lib/components/Tooltip/docs/index.story.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import STooltip from "../index.vue";
+</script>
+
+<template>
+  <Story title="Date Display/Tooltip">
+    <div class="container">
+      <STooltip title="Tooltip text">
+        <span class="text">Hover over me!</span>
+      </STooltip>
+    </div>
+  </Story>
+</template>
+
+<style scoped>
+.container {
+  margin: 50px;
+}
+
+.text {
+  border-bottom: 1px dotted black;
+}
+</style>
