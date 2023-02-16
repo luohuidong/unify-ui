6c639f36eb060e981ab2319cbe5fec46f1a44e85
luohuidong <luohuidong01@126.com>
2023-02-17 00:35:47 +0800

    refactor: refactor Tooltip

--- src/lib/components/Tooltip/index.vue
+++ src/lib/components/Tooltip/index.vue
@@ -7,157 +7,21 @@ export default defineComponent({
 </script>
 
 <script lang="ts" setup>
-import { ref, onMounted, onUnmounted } from "vue";
-import { computePosition, flip, shift, offset, arrow, autoUpdate } from "@floating-ui/dom";
+import { Propup } from "@/lib/components";
 
-import FloatingElement from "./FloatingElement.vue";
-
-const props = defineProps<{
+defineProps<{
   title: string;
   placement: "top" | "bottom" | "left" | "right";
   appendToBody?: boolean;
-  overlayClassName?: string;
 }>();
-
-const referenceRef = ref<Element | null>(null);
-const floatingRef = ref<HTMLElement | null>(null);
-function setFloatingRef(e: HTMLElement) {
-  floatingRef.value = e;
-}
-const floatingArrowRef = ref<HTMLElement | null>(null);
-function setFloatingArrowRef(e: HTMLElement) {
-  floatingArrowRef.value = e;
-}
-
-function updatePosition() {
-  const reference = referenceRef.value;
-  const floating = floatingRef.value;
-  const floatingArrow = floatingArrowRef.value;
-
-  if (reference && floating && floatingArrow) {
-    computePosition(reference, floating, {
-      placement: props.placement,
-      middleware: [offset(6), flip(), shift({ padding: 5 }), arrow({ element: floatingArrow })],
-    }).then(({ x, y, middlewareData, placement }) => {
-      Object.assign(floating.style, {
-        left: `${x}px`,
-        top: `${y}px`,
-      });
-
-      if (middlewareData.arrow) {
-        const { x: arrowX, y: arrowY } = middlewareData.arrow;
-
-        const staticSide = {
-          top: "bottom",
-          right: "left",
-          bottom: "top",
-          left: "right",
-        }[placement.split("-")[0]] as "top" | "right" | "bottom" | "left";
-
-        Object.assign(floatingArrow.style, {
-          left: arrowX != null ? `${arrowX}px` : "",
-          top: arrowY != null ? `${arrowY}px` : "",
-          right: "",
-          bottom: "",
-          [staticSide]: "-4px",
-        });
-      }
-    });
-  }
-}
-
-let cleanup: (() => void) | null = null;
-
-function showTooltip() {
-  const reference = referenceRef.value;
-  const floating = floatingRef.value;
-
-  if (reference && floating) {
-    floating.style.display = "block";
-    cleanup = autoUpdate(reference, floating, updatePosition);
-  }
-
-  updatePosition();
-}
-
-function hideTooltip() {
-  const reference = referenceRef.value;
-  const floating = floatingRef.value;
-
-  if (reference && floating) {
-    floating.style.display = "";
-    cleanup && cleanup();
-  }
-}
-
-onMounted(() => {
-  const reference = referenceRef.value;
-  if (reference) {
-    reference.addEventListener("mouseenter", showTooltip);
-    reference.addEventListener("mouseleave", hideTooltip);
-    reference.addEventListener("focus", showTooltip);
-    reference.addEventListener("blur", hideTooltip);
-  }
-});
-
-onUnmounted(() => {
-  const reference = referenceRef.value;
-  if (reference) {
-    reference.removeEventListener("mouseenter", showTooltip);
-    reference.removeEventListener("mouseleave", hideTooltip);
-    reference.removeEventListener("focus", showTooltip);
-    reference.removeEventListener("blur", hideTooltip);
-  }
-});
 </script>
 
 <template>
-  <div ref="referenceRef" :class="$style.container">
+  <Propup :overlay-append-to-body="appendToBody" :overlay-placement="placement">
     <slot></slot>
 
-    <Teleport v-if="appendToBody" to="body">
-      <FloatingElement
-        :set-floating-ref="setFloatingRef"
-        :set-floating-arrow-ref="setFloatingArrowRef"
-        :title="title"
-        :overlay-class-name="overlayClassName"
-      ></FloatingElement>
-    </Teleport>
-
-    <FloatingElement
-      v-else
-      :set-floating-ref="setFloatingRef"
-      :set-floating-arrow-ref="setFloatingArrowRef"
-      :title="title"
-      :overlay-class-name="overlayClassName"
-    ></FloatingElement>
-  </div>
+    <template #content>
+      {{ title }}
+    </template>
+  </Propup>
 </template>
-
-<style module>
-.container {
-  display: inline-block;
-}
-
-.tooltip {
-  display: none;
-  width: max-content;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background: #222;
-  color: white;
-  font-weight: bold;
-  padding: 5px;
-  border-radius: 4px;
-  font-size: 90%;
-}
-
-.arrow {
-  position: absolute;
-  background: #222;
-  width: 8px;
-  height: 8px;
-  transform: rotate(45deg);
-}
-</style>
