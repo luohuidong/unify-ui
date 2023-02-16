dc3d84ad1e468c564cc1410f2ad98768133236a4
luohuidong <luohuidong01@126.com>
2023-02-16 13:02:36 +0800

    refactor: refactor tooltip

--- src/lib/components/Tooltip/docs/index.story.vue
+++ src/lib/components/Tooltip/docs/index.story.vue
@@ -4,20 +4,32 @@ import STooltip from "../index.vue";
 
 <template>
   <Story title="Date Display/Tooltip">
-    <div class="container">
-      <STooltip title="Tooltip text">
-        <span class="text">Hover over me!</span>
-      </STooltip>
-    </div>
+    <STooltip v-slot="{ setRef }" title="Tooltip text" placement="top">
+      <button :ref="(el) => setRef(el as Element)" class="button">Hover over me!</button>
+    </STooltip>
+
+    <STooltip v-slot="{ setRef }" title="Tooltip text" placement="top" append-to-body>
+      <button :ref="(el) => setRef(el as Element)" class="button">Tooltip append to body</button>
+    </STooltip>
   </Story>
 </template>
 
 <style scoped>
 .container {
   margin: 50px;
+  height: 400px;
+  border: 1px solid grey;
+  overflow: scroll;
 }
 
-.text {
-  border-bottom: 1px dotted black;
+.inner-container {
+  height: 800px;
+}
+
+.button {
+  background: gainsboro;
+  border: none;
+  padding: 20px;
+  margin: 10px;
 }
 </style>
--- src/lib/components/Tooltip/index.vue
+++ src/lib/components/Tooltip/index.vue
@@ -7,65 +7,149 @@ export default defineComponent({
 </script>
 
 <script lang="ts" setup>
+import { ref, onMounted, onUnmounted } from "vue";
+import {
+  computePosition,
+  flip,
+  shift,
+  offset,
+  arrow,
+  autoUpdate,
+  type Placement,
+} from "@floating-ui/dom";
+
 defineProps<{
   title: string;
+  placement: Placement;
+  appendToBody?: boolean;
 }>();
+
+const referenceRef = ref<Element | null>(null);
+const floatingRef = ref<HTMLElement | null>(null);
+const floatingArrowRef = ref<HTMLElement | null>(null);
+
+function setRef(el: Element | null) {
+  referenceRef.value = el;
+}
+
+function updatePosition() {
+  const reference = referenceRef.value;
+  const floating = floatingRef.value;
+  const floatingArrow = floatingArrowRef.value;
+
+  if (reference && floating && floatingArrow) {
+    computePosition(reference, floating, {
+      placement: "top",
+      middleware: [offset(6), flip(), shift({ padding: 5 }), arrow({ element: floatingArrow })],
+    }).then(({ x, y, middlewareData, placement }) => {
+      Object.assign(floating.style, {
+        left: `${x}px`,
+        top: `${y}px`,
+      });
+
+      if (middlewareData.arrow) {
+        const { x: arrowX, y: arrowY } = middlewareData.arrow;
+
+        const staticSide = {
+          top: "bottom",
+          right: "left",
+          bottom: "top",
+          left: "right",
+        }[placement.split("-")[0]] as "top" | "right" | "bottom" | "left";
+
+        Object.assign(floatingArrow.style, {
+          left: arrowX != null ? `${arrowX}px` : "",
+          top: arrowY != null ? `${arrowY}px` : "",
+          right: "",
+          bottom: "",
+          [staticSide]: "-4px",
+        });
+      }
+    });
+  }
+}
+
+let cleanup: (() => void) | null = null;
+
+function showTooltip() {
+  const reference = referenceRef.value;
+  const floating = floatingRef.value;
+
+  if (reference && floating) {
+    floating.style.display = "block";
+    cleanup = autoUpdate(reference, floating, updatePosition);
+  }
+
+  updatePosition();
+}
+
+function hideTooltip() {
+  const reference = referenceRef.value;
+  const floating = floatingRef.value;
+
+  if (reference && floating) {
+    floating.style.display = "";
+    cleanup && cleanup();
+  }
+}
+
+onMounted(() => {
+  const reference = referenceRef.value;
+  if (reference) {
+    reference.addEventListener("mouseenter", showTooltip);
+    reference.addEventListener("mouseleave", hideTooltip);
+    reference.addEventListener("focus", showTooltip);
+    reference.addEventListener("blur", hideTooltip);
+  }
+});
+
+onUnmounted(() => {
+  const reference = referenceRef.value;
+  if (reference) {
+    reference.removeEventListener("mouseenter", showTooltip);
+    reference.removeEventListener("mouseleave", hideTooltip);
+    reference.removeEventListener("focus", showTooltip);
+    reference.removeEventListener("blur", hideTooltip);
+  }
+});
 </script>
 
 <template>
-  <div :class="$style.tooltip">
-    <slot></slot>
-    <span :class="$style.title">{{ title }}</span>
+  <slot :set-ref="setRef"></slot>
+
+  <Teleport v-if="appendToBody" to="body">
+    <div ref="floatingRef" class="tooltip">
+      {{ title }}
+      <div ref="floatingArrowRef" class="arrow"></div>
+    </div>
+  </Teleport>
+
+  <div v-else ref="floatingRef" class="tooltip">
+    {{ title }}
+    <div ref="floatingArrowRef" class="arrow"></div>
   </div>
 </template>
 
-<style lang="scss" module>
-/* Tooltip container */
+<style scoped>
 .tooltip {
-  position: relative;
-  display: inline-block;
-}
-
-/* Show the tooltip text when you mouse over the tooltip container */
-.tooltip:hover .title {
-  visibility: visible;
-  opacity: 1;
-}
-
-/* Tooltip text */
-.title {
-  visibility: hidden;
-  padding: 5px 10px;
-
-  /* Position the tooltip text */
+  display: none;
+  width: max-content;
   position: absolute;
-  z-index: 1;
-  bottom: 100%;
-  left: 50%;
-  transform: translateX(-50%);
+  top: 0;
+  left: 0;
+  background: #222;
+  color: white;
+  font-weight: bold;
+  padding: 5px;
+  border-radius: 4px;
+  font-size: 90%;
+}
 
-  background-color: #555;
-  color: #fff;
-  text-align: center;
-  border-radius: 2px;
-  white-space: nowrap;
-
-  /* Fade in tooltip */
-  opacity: 0;
-  transition: opacity 0.4s;
-
-  /* Tooltip arrow */
-  &::after {
-    content: "";
-
-    position: absolute;
-    top: 100%;
-    left: 50%;
-    transform: translateX(-50%);
-
-    border-width: 5px;
-    border-style: solid;
-    border-color: #555 transparent transparent transparent;
-  }
+.arrow {
+  position: absolute;
+  background: #222;
+  width: 8px;
+  height: 8px;
+  transform: rotate(45deg);
 }
 </style>
