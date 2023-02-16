cd1e6ed79ecdf268ec105243d6eb339d4a7c632d
luohuidong <luohuidong01@126.com>
2023-02-16 17:57:48 +0800

    refactor: extract Tooltip floating element into SFC

---
+++ src/lib/components/Tooltip/FloatingElement.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps<{
+  overlayClassName?: string;
+  title: string;
+  setFloatingRef: (el: HTMLElement) => void;
+  setFloatingArrowRef: (el: HTMLElement) => void;
+}>();
+</script>
+
+<template>
+  <div
+    :ref="(e) => setFloatingRef(e as HTMLDivElement)"
+    :class="[$style.tooltip, overlayClassName]"
+  >
+    {{ title }}
+    <div :ref="(e)=>setFloatingArrowRef(e as HTMLDivElement)" :class="$style.arrow"></div>
+  </div>
+</template>
+
+<style module>
+.tooltip {
+  display: none;
+  width: max-content;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: #222;
+  color: white;
+  font-weight: bold;
+  padding: 5px;
+  border-radius: 4px;
+  font-size: 90%;
+}
+
+.arrow {
+  position: absolute;
+  background: #222;
+  width: 8px;
+  height: 8px;
+  transform: rotate(45deg);
+}
+</style>
