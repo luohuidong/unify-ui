068faeb0cc24591953d612f37201b8b75ba655f8
luohuidong <luohuidong01@126.com>
2023-02-20 15:53:22 +0800

    refactor: refactor thead component

--- src/components/Table/EzThead.vue
+++ src/components/Table/EzThead.vue
@@ -1,13 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useInject } from "./useInject";
+
+const { rootProps } = useInject();
+</script>
 
 <template>
   <thead :class="$style.thead">
-    <slot></slot>
+    <tr>
+      <th v-for="col in rootProps.columns" :key="col.key" scoop="column" :class="$style.th">
+        {{ col.title }}
+      </th>
+    </tr>
   </thead>
 </template>
 
 <style lang="scss" module>
 .thead {
-  background: #fafafa;
+  background: #f5f6fa;
+  color: #666666;
+}
+
+.th {
+  white-space: nowrap;
+  padding: 16px;
+  text-align: left;
+  font-size: 14px;
+  font-weight: 500;
 }
 </style>
--- src/components/Table/types.ts
+++ src/components/Table/types.ts
@@ -1,13 +1,20 @@
 export type Record = any;
 
 export interface RootProps {
+  /** 数据唯一索引 */
   rowKey: string;
+  data: Record[];
   columns: {
     key: string;
     title: string;
+    sortable?: boolean;
   }[];
   rowExpand?: {
-    expandCondition: (record: Record) => boolean;
+    expandCondition: (Record: Record) => boolean;
   };
-  data: Record[];
+  sort?: {
+    columnKey: string;
+    order: "ascending" | "descending";
+  };
+  showFoot?: boolean;
 }
