70820637147ada0650f9117a59046628364f3dc4
luohuidong <luohuidong01@126.com>
2023-02-20 16:27:26 +0800

    refactor: refactor table component

--- src/components/Table/EzTfoot.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <tfoot>
-    <slot></slot>
-  </tfoot>
-</template>
-
-<style lang="scss" module></style>
--- src/components/Table/EzTr.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <tr>
-    <slot></slot>
-  </tr>
-</template>
-
-<style lang="scss" module></style>
