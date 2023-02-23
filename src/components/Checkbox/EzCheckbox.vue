a86831b713fd95ce28c9bb3fb658e9674c4c3133
luohuidong <luohuidong01@126.com>
2023-02-24 01:45:35 +0800

    feat: add indeterminate property which can help you to achieve a 'check all' effect

--- src/components/Checkbox/EzCheckbox.vue
+++ src/components/Checkbox/EzCheckbox.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { computed } from "vue";
 
+import Tick from "./icons/TickIcon.vue";
+import IndeterminateIcon from "./icons/IndeterminateIcon.vue";
+
 const props = withDefaults(
   defineProps<{
     label?: string;
-    modelValue?: boolean;
+    modelValue: boolean;
     disabled?: boolean;
-    partialSelection?: boolean;
+    indeterminate?: boolean;
   }>(),
   {
     label: "",
@@ -39,12 +42,31 @@ const cursor = computed(() => (props.disabled ? "not-allowed" : "pointer"));
       :disabled="disabled"
       @change="handleChange"
     />
-    <span :class="[$style.checkmark, { [$style.partialSelection]: partialSelection }]"></span>
+    <div v-if="indeterminate" :class="$style.indeterminate">
+      <IndeterminateIcon></IndeterminateIcon>
+    </div>
+    <div v-else :class="$style.checkmark">
+      <Tick></Tick>
+    </div>
+
     <span v-if="label" :class="$style.label">{{ label }}</span>
   </label>
 </template>
 
 <style lang="scss" module>
+$active-color: #037aff;
+$hover-color: #0261cc;
+%square {
+  height: 14px;
+  width: 14px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 2px;
+}
+
 .container {
   height: 26px;
   display: inline-flex;
@@ -58,85 +80,56 @@ const cursor = computed(() => (props.disabled ? "not-allowed" : "pointer"));
   display: none;
 }
 
-// custom checkbox
-.checkmark {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 14px;
-  width: 14px;
-  border: 1px solid rgba(206, 206, 206, 1);
-  border-radius: 2px;
+.indeterminate {
+  @extend %square;
+
+  color: white;
+  background: $active-color;
+  border: 1px solid $active-color;
+
+  &:hover {
+    background-color: $hover-color;
+    border-color: $hover-color;
+  }
 }
 
-// When the checkbox is checked, add a blue background
-.input:not(:disabled):checked ~ .checkmark {
-  background-color: #037aff;
-  border: 1px solid #037aff;
+// custom checkbox
+.checkmark {
+  @extend %square;
+
+  color: white;
+  background: white;
+  border: 1px solid rgba(206, 206, 206, 1);
 }
 
 // When the checkbox is disabled, add grey background color
 .input:disabled ~ .checkmark {
-  cursor: not-allowed;
+  color: #a8abb2;
   background-color: #f4f4f4;
+  cursor: not-allowed;
+}
+
+// When the checkbox is checked, add a blue background and a white tick
+.input:not(:disabled):checked ~ .checkmark {
+  color: white;
+  background: $active-color;
+  border: 1px solid $active-color;
 }
 
 // On mouse-over, add a background color
 .container:hover > .input:not(:disabled) {
   // When the checkbox is checked, add a blue background
   &:checked ~ .checkmark {
-    background-color: #0261cc;
+    background-color: $hover-color;
   }
 
   // When the checkbox is not checked, add a grey background
   &:not(:checked) ~ .checkmark {
-    border: 1px solid #037aff;
+    border: 1px solid $hover-color;
   }
-}
-
-/* Create the checkmark/indicator (hidden when not checked) */
-.container > .checkmark::after {
-  opacity: 0;
-  content: " ";
-  width: 2px;
-  height: 6px;
-  border: solid white;
-  border-width: 0 2px 2px 0;
-  transform: rotate(45deg);
-}
-
-/* Show the checkmark when checked */
-.container > input:checked ~ .checkmark::after {
-  opacity: 1;
-}
-.container > input:checked:disabled ~ .checkmark::after {
-  opacity: 1;
-  border-color: #dcdcdc;
 }
 
 .label {
   padding: 0 8px;
-}
-
-.partialSelection::after {
-  opacity: 0;
-  content: " ";
-  width: 2px;
-  height: 1px;
-  background: white;
-  transform: rotate(45deg);
-}
-
-input:not(:checked) ~ .partialSelection {
-  background-color: #0261cc;
-  border: 1px solid #037aff;
-
-  &::after {
-    content: " ";
-    width: 2px;
-    height: 1px;
-    background: white;
-    transform: rotate(45deg);
-  }
 }
 </style>
