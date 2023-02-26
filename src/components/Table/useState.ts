dca170c0af0668a74f5694b55de65ab3076be678
luohuidong <luohuidong01@126.com>
2023-02-26 11:39:36 +0800

    refactor: change table selection api

--- src/components/Table/useState.ts
+++ src/components/Table/useState.ts
@@ -1,12 +1,11 @@
 import { reactive, provide } from "vue";
 
-import type { Key, Record, RootState } from "./types";
+import type { Key, RootState } from "./types";
 import * as injectKeys from "./injectKeys";
 
 export function useState() {
   const state: RootState = reactive({
     selectedRowKeys: new Set<Key>(),
-    selectedRows: new Map<Key, Record>(),
   });
 
   provide(injectKeys.rootStateKey, state);
