c79166d992d77c0c7e37f744791819ccd0ccf4a9
luohuidong <luohuidong01@126.com>
2023-02-27 20:32:48 +0800

    refactor: define all set utils function inside SetUtils class

--- src/components/Table/utils.ts
+++ src/components/Table/utils.ts
@@ -1,11 +1,16 @@
-export function intersection(set1: Set<string | number>, set2: Set<string | number>) {
-  const tmpSet = new Set<string | number>();
+type SetType = Set<string | number>;
 
-  for (let i of set1) {
-    if (set2.has(i)) {
-      tmpSet.add(i);
-    }
+export class SetUtils {
+  static intersection(set1: SetType, set2: SetType) {
+    return new Set([...set1].filter((item) => set2.has(item)));
   }
 
-  return tmpSet;
+  static difference(set1: SetType, set2: SetType) {
+    return new Set([...set1].filter((item) => !set2.has(item)));
+  }
+
+  static equal(set1: SetType, set2: SetType) {
+    const tmpSet = SetUtils.difference(set1, set2);
+    return tmpSet.size === 0 && set1.size === set2.size;
+  }
 }
