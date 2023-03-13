7d1f9405195a1ffd80d522eb371900e4dc00debe
luohuidong <luohuidong01@126.com>
2023-03-13 17:44:43 +0800

    build: remove vite-plugin-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import path from "path";
-import Pages from "vite-plugin-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    Pages({
-      dirs: "src/examples",
-      importMode: "sync",
-      exclude: ["**/Case*.vue", "**/use*.ts"],
-    }),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       "@": path.join(__dirname, "src"),
