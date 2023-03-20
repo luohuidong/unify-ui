659f2af36b5fde57dec6c9844bf006aeae1649af
luohuidong <luohuidong01@126.com>
2023-03-20 23:56:23 +0800

    build: add the logic of building for production

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import path from "path";
 
+import packagejson from "./package.json";
+
+const peerdependencies = Object.keys(packagejson.peerDependencies);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
@@ -19,5 +23,17 @@ export default defineConfig({
     // simulate DOM with happy-dom
     // (requires installing happy-dom as a peer dependency)
     environment: "happy-dom",
+  },
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: path.resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: peerdependencies,
+    },
   },
 });
