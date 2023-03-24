482a3e0299609e0d66cf1c979dbd32b211d917cf
luohuidong <luohuidong01@126.com>
2023-03-24 23:53:26 +0800

    feat: add radio component

---
+++ src/components/Radio/docs/index.story.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import EzRadio from "../EzRadio.vue";
+</script>
+
+<template>
+  <Story title="Data Entry/Radio">
+    <Variant title="Normal use">
+      <div class="container">
+        <EzRadio name="radio" title="one"></EzRadio>
+        <EzRadio name="radio" title="two"></EzRadio>
+        <EzRadio name="radio" title="three"></EzRadio>
+        <EzRadio name="radio" title="four"></EzRadio>
+      </div>
+    </Variant>
+  </Story>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+  gap: 10px;
+}
+</style>
