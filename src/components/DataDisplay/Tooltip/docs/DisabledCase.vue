7bfd7318fc184a4598603bc2159446b09a78f0ab
luohuidong <luohuidong01@126.com>
2023-03-27 23:50:45 +0800

    feat: tooltip can be disabled

---
+++ src/components/DataDisplay/Tooltip/docs/DisabledCase.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { EzButton } from "@/components";
+import EzTooltip from "../index.vue";
+
+const disabled = ref(false);
+function handleClick() {
+  disabled.value = !disabled.value;
+}
+</script>
+
+<template>
+  <EzButton @click="handleClick">Click button to change tooltip disabled</EzButton>
+
+  <div>
+    <EzTooltip title="Disabled Case" placement="bottom" :disabled="disabled">
+      <div>Tooltip disabled: {{ disabled }}</div>
+    </EzTooltip>
+  </div>
+</template>
+
+<style lang="scss" module></style>
--- src/components/DataDisplay/Tooltip/docs/index.story.vue
+++ src/components/DataDisplay/Tooltip/docs/index.story.vue
@@ -1,44 +1,18 @@
 <script setup lang="ts">
-import { EzButton } from "@/components";
-import STooltip from "../index.vue";
+import NormalUseCase from "./NormalUseCase.vue";
+import DisabledCase from "./DisabledCase.vue";
 </script>
 
 <template>
   <Story title="Date Display/Tooltip">
-    <div class="container">
-      <div>
-        <STooltip title="Tooltip text" placement="top">
-          <EzButton>Hover over me!</EzButton>
-        </STooltip>
-      </div>
+    <Variant title="Normal use">
+      <NormalUseCase></NormalUseCase>
+    </Variant>
 
-      <div>
-        <STooltip title="Tooltip text" placement="bottom" append-to-body>
-          <EzButton>Tooltip append to body</EzButton>
-        </STooltip>
-      </div>
-
-      <div>
-        <STooltip title="Tooltip text" placement="left">
-          <EzButton>Hover over me!</EzButton>
-        </STooltip>
-      </div>
-
-      <div>
-        <STooltip title="Tooltip text" placement="right" append-to-body>
-          <EzButton>Tooltip append to body</EzButton>
-        </STooltip>
-      </div>
-    </div>
+    <Variant title="Disabled Case">
+      <DisabledCase></DisabledCase>
+    </Variant>
   </Story>
 </template>
 
-<style scoped>
-.container {
-  display: grid;
-  grid-template-columns: 400px 400px;
-  grid-template-rows: 400px;
-  align-items: center;
-  justify-items: center;
-}
-</style>
+<style scoped></style>
