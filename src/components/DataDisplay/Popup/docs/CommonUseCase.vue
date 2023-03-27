dc09ef062c805d1ef55d088c11efff0d10c89452
luohuidong <luohuidong01@126.com>
2023-03-27 21:35:02 +0800

    docs: change popup docs

---
+++ src/components/DataDisplay/Popup/docs/CommonUseCase.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { EzButton } from "@/components";
+import Popup from "../index.vue";
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      <Popup placement="top">
+        <EzButton>top black</EzButton>
+
+        <template #content>
+          <div class="box">top</div>
+        </template>
+      </Popup>
+    </div>
+
+    <div>
+      <Popup placement="bottom" append-to-body background-color="green">
+        <EzButton>bottom green</EzButton>
+
+        <template #content>
+          <div class="box">bottom</div>
+        </template>
+      </Popup>
+    </div>
+
+    <div>
+      <Popup placement="left" background-color="red">
+        <EzButton>left red </EzButton>
+
+        <template #content>
+          <div class="box">bottom</div>
+        </template>
+      </Popup>
+    </div>
+
+    <div>
+      <Popup placement="right" background-color="blue">
+        <EzButton>right blue</EzButton>
+
+        <template #content>
+          <div class="box">right</div>
+        </template>
+      </Popup>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: grid;
+  grid-template-columns: 400px 400px;
+  grid-template-rows: 400px;
+  align-items: center;
+  justify-items: center;
+}
+
+.box {
+  height: 100px;
+  width: 100px;
+}
+</style>
