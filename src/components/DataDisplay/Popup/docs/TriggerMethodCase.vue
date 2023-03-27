b75529a590d6203c1a8ca554f6576a154a66fa65
luohuidong <luohuidong01@126.com>
2023-03-27 21:35:02 +0800

    docs: change popup docs

---
+++ src/components/DataDisplay/Popup/docs/TriggerMethodCase.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { EzButton } from "@/components";
+import EzPopup from "../index.vue";
+
+const visible = ref(true);
+function handleClick() {
+  visible.value = !visible.value;
+}
+</script>
+
+<template>
+  <div :style="{ height: '100vh' }">
+    <div :style="{ marginBottom: '100px' }">
+      <EzPopup placement="bottom" trigger="click" append-to-body>
+        <EzButton>Trigger: click</EzButton>
+
+        <template #content>
+          <div class="box">bottom</div>
+        </template>
+      </EzPopup>
+    </div>
+
+    <div :style="{ marginBottom: '100px' }">
+      <EzPopup placement="bottom" append-to-body>
+        <EzButton>Trigger: hover</EzButton>
+
+        <template #content>
+          <div class="box">bottom</div>
+        </template>
+      </EzPopup>
+    </div>
+
+    <div>
+      <EzPopup placement="bottom" append-to-body trigger="controlled" :visible="visible">
+        <EzButton @click="handleClick">Trigger: Controlled</EzButton>
+
+        <template #content>
+          <div class="box">bottom</div>
+        </template>
+      </EzPopup>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.box {
+  height: 100px;
+  width: 100px;
+}
+</style>
--- src/components/DataDisplay/Popup/docs/index.story.vue
+++ src/components/DataDisplay/Popup/docs/index.story.vue
@@ -1,99 +1,18 @@
 <script setup lang="ts">
-import { EzButton } from "@/components";
-
-import Popup from "../index.vue";
-import VisibleCase from "./VisibleCase.vue";
+import CommonUseCase from "./CommonUseCase.vue";
+import TriggerMethod from "./TriggerMethodCase.vue";
 </script>
 
 <template>
   <Story title="Date Display/Popup">
     <Variant title="default">
-      <div class="container">
-        <div>
-          <Popup placement="top">
-            <EzButton>top black</EzButton>
-
-            <template #content>
-              <div class="box">top</div>
-            </template>
-          </Popup>
-        </div>
-
-        <div>
-          <Popup placement="bottom" append-to-body background-color="green">
-            <EzButton>bottom green</EzButton>
-
-            <template #content>
-              <div class="box">bottom</div>
-            </template>
-          </Popup>
-        </div>
-
-        <div>
-          <Popup placement="left" background-color="red">
-            <EzButton>left red </EzButton>
-
-            <template #content>
-              <div class="box">bottom</div>
-            </template>
-          </Popup>
-        </div>
-
-        <div>
-          <Popup placement="right" background-color="blue">
-            <EzButton>right blue</EzButton>
-
-            <template #content>
-              <div class="box">right</div>
-            </template>
-          </Popup>
-        </div>
-      </div>
+      <CommonUseCase></CommonUseCase>
     </Variant>
 
     <Variant title="Trigger method">
-      <div :style="{ height: '100vh' }">
-        <div :style="{ marginBottom: '100px' }">
-          <Popup placement="bottom" trigger="click" append-to-body>
-            <EzButton>Trigger: click</EzButton>
-
-            <template #content>
-              <div class="box">bottom</div>
-            </template>
-          </Popup>
-        </div>
-
-        <div>
-          <Popup placement="bottom" append-to-body>
-            <EzButton>Trigger: hover</EzButton>
-
-            <template #content>
-              <div class="box">bottom</div>
-            </template>
-          </Popup>
-        </div>
-      </div>
-    </Variant>
-
-    <Variant title="Controlled Mode">
-      <div :style="{ height: '100vh' }">
-        <VisibleCase></VisibleCase>
-      </div>
+      <TriggerMethod></TriggerMethod>
     </Variant>
   </Story>
 </template>
 
-<style scoped>
-.container {
-  display: grid;
-  grid-template-columns: 400px 400px;
-  grid-template-rows: 400px;
-  align-items: center;
-  justify-items: center;
-}
-
-.box {
-  height: 100px;
-  width: 100px;
-}
-</style>
+<style scoped></style>
