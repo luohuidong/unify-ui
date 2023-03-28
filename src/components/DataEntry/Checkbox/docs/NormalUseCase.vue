92e80f5cd6793e3a40a0d6aedd9647674219f59b
luohuidong <luohuidong01@126.com>
2023-03-28 23:00:46 +0800

    docs: update checkbox docs

---
+++ src/components/DataEntry/Checkbox/docs/NormalUseCase.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import EzCheckbox from "../EzCheckbox.vue";
+
+const checked1 = ref(true);
+const checked2 = ref(false);
+const checked3 = ref(true);
+const checked4 = ref(false);
+</script>
+
+<template>
+  <EzCheckbox v-model:checked="checked1" label="checked"></EzCheckbox>
+  <EzCheckbox v-model:checked="checked2" label="not checked"></EzCheckbox>
+  <EzCheckbox v-model:checked="checked3" label="checked & disabled" disabled></EzCheckbox>
+  <EzCheckbox v-model:checked="checked4" label="not checked & disabled" disabled></EzCheckbox>
+</template>
+
+<style lang="scss" module></style>
--- src/components/DataEntry/Checkbox/docs/index.story.vue
+++ src/components/DataEntry/Checkbox/docs/index.story.vue
@@ -1,55 +1,36 @@
 <script setup lang="ts">
-import { ref } from "vue";
-
 import EzCheckbox from "../EzCheckbox.vue";
-import EzCheckboxGroup from "../EzCheckboxGroup.vue";
 
-const checked1 = ref(true);
-const checked2 = ref(false);
-const checked3 = ref(false);
-
-const options = [
-  {
-    label: "test1",
-    value: "test1",
-  },
-  {
-    label: "test2",
-    value: "test2",
-  },
-  {
-    label: "test3",
-    value: "test3",
-  },
-];
-
-const checkboxGroupValue = ref(new Set<string>());
+import NormalUseCase from "./NormalUseCase.vue";
+import CheckboxGroupCase from "./CheckboxGroupCase.vue";
 </script>
 
 <template>
   <Story title="Data Entry/Checkbox">
     <Variant title="Normal use">
-      <EzCheckbox v-model="checked1" label="One"></EzCheckbox>
-      <EzCheckbox v-model="checked2" label="Two"></EzCheckbox>
-      <EzCheckbox v-model="checked2" label="Two" disabled></EzCheckbox>
+      <NormalUseCase></NormalUseCase>
     </Variant>
 
     <Variant title="No label">
-      <EzCheckbox v-model="checked2"></EzCheckbox>
+      <ul>
+        <li>checked: <EzCheckbox :checked="true"></EzCheckbox></li>
+        <li>not checked: <EzCheckbox :checked="false"></EzCheckbox></li>
+      </ul>
     </Variant>
 
     <Variant title="Indeterminate">
-      <EzCheckbox v-model="checked3" indeterminate></EzCheckbox>
+      <ul>
+        <li>check: <EzCheckbox :checked="true" indeterminate></EzCheckbox></li>
+        <li>not checked:<EzCheckbox :checked="false" indeterminate></EzCheckbox></li>
+        <li>check & disabled <EzCheckbox :checked="true" indeterminate></EzCheckbox></li>
+        <li>not check & disabled <EzCheckbox :checked="false" indeterminate></EzCheckbox></li>
+      </ul>
     </Variant>
 
     <Variant title="Checkbox Group">
-      <EzCheckboxGroup v-model="checkboxGroupValue" :options="options"></EzCheckboxGroup>
+      <CheckboxGroupCase></CheckboxGroupCase>
     </Variant>
   </Story>
 </template>
 
-<style scoped>
-.container {
-  height: 100px;
-}
-</style>
+<style scoped></style>
