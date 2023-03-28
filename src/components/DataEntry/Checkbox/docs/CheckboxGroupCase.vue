f65c57377a71b75b604f764bd4981cc2a0256fd8
luohuidong <luohuidong01@126.com>
2023-03-28 23:00:46 +0800

    docs: update checkbox docs

---
+++ src/components/DataEntry/Checkbox/docs/CheckboxGroupCase.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import EzCheckboxGroup from "../EzCheckboxGroup.vue";
+
+const options = [
+  {
+    label: "test1",
+    value: "test1",
+  },
+  {
+    label: "test2",
+    value: "test2",
+  },
+  {
+    label: "test3",
+    value: "test3",
+  },
+];
+
+const checkboxGroupValue = ref(new Set<string>());
+</script>
+
+<template>
+  <ul>
+    <li>
+      normal checkbox group:
+      <EzCheckboxGroup v-model="checkboxGroupValue" :options="options"></EzCheckboxGroup>
+    </li>
+    <li>
+      disabled checkbox group:
+      <EzCheckboxGroup v-model="checkboxGroupValue" :options="options"></EzCheckboxGroup>
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" module></style>
