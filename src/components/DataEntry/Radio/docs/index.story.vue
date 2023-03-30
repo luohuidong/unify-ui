1b2282d89e3ef7763afe02496144a343f3d601f8
luohuidong <luohuidong01@126.com>
2023-03-31 00:21:09 +0800

    refactor: radio's value prop must be passed

--- src/components/DataEntry/Radio/docs/index.story.vue
+++ src/components/DataEntry/Radio/docs/index.story.vue
@@ -6,10 +6,10 @@ import EzRadio from "../EzRadio.vue";
   <Story title="Data Entry/Radio">
     <Variant title="Normal use">
       <div class="container">
-        <EzRadio name="radio" title="one"></EzRadio>
-        <EzRadio name="radio" title="two"></EzRadio>
-        <EzRadio name="radio" title="three"></EzRadio>
-        <EzRadio name="radio" title="four"></EzRadio>
+        <EzRadio name="radio" title="one" value="1"></EzRadio>
+        <EzRadio name="radio" title="two" value="2"></EzRadio>
+        <EzRadio name="radio" title="three" value="3"></EzRadio>
+        <EzRadio name="radio" title="four" value="4"></EzRadio>
       </div>
     </Variant>
   </Story>
