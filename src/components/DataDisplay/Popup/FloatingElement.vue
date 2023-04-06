2c7385054891b49de631e17e650220e0d3fe2d66
luohuidong <luohuidong01@126.com>
2023-04-06 21:29:09 +0800

    refactor: popup show box shadow

--- src/components/DataDisplay/Popup/FloatingElement.vue
+++ src/components/DataDisplay/Popup/FloatingElement.vue
@@ -50,6 +50,8 @@ const color = computed(() => rootPropsKey?.fontColor || "white");
   border-radius: 4px;
   font-size: 90%;
   z-index: zindex.$popover;
+  box-shadow: 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 3px 6px -4px rgba(0, 0, 0, 0.12),
+    0 9px 28px 8px rgba(0, 0, 0, 0.05);
 }
 
 .arrow {
