848a1ebcc363456255d4f61801f9cadc4b0c9ac5
luohuidong <luohuidong01@126.com>
2023-04-09 16:50:27 +0800

    refactor: change docs sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,3 +1,15 @@
 export default {
   title: "Easy UI",
+  themeConfig: {
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2022-present luohuidong",
+    },
+    sidebar: [
+      {
+        text: "Guide",
+        items: [{ text: "Getting Started", link: "/guide/getting-started" }],
+      },
+    ],
+  },
 };
