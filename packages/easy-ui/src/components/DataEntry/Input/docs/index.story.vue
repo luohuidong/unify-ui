4e03291a8a8989dc409ed21ff6cfe6ea6a2e9007
luohuidong <luohuidong01@126.com>
2023-04-14 23:59:06 +0800

    feat: the Input component can add a leading add-on

--- packages/easy-ui/src/components/DataEntry/Input/docs/index.story.vue
+++ packages/easy-ui/src/components/DataEntry/Input/docs/index.story.vue
@@ -10,17 +10,24 @@ const value = ref("allan@mail.com");
     <Variant title="Basic usage">
       <div class="container">
         <EzInput v-model="value" />
-        <EzInput placeholder="you@email.com"></EzInput>
+        <EzInput placeholder="placeholder: you@email.com"></EzInput>
         <EzInput v-model="value" disabled></EzInput>
       </div>
+    </Variant>
+
+    <Variant title="Inline leading add-on">
+      <EzInput>
+        <template #inline-leading-add-on>
+          <span class="inline-leading-add-on">http://</span>
+        </template>
+      </EzInput>
     </Variant>
   </Story>
 </template>
 
 <style lang="scss" scoped>
 .container {
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
+  height: 100vh;
+  padding: 20px;
 }
 </style>
--- packages/easy-ui/src/components/DataEntry/Input/index.vue
+++ packages/easy-ui/src/components/DataEntry/Input/index.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, useSlots } from "vue";
 
 export default defineComponent({
   name: "EzInput",
@@ -23,42 +23,81 @@ function handleInput(e: Event) {
 </script>
 
 <template>
-  <input
-    :value="modelValue"
-    class="input"
-    :placeholder="placeholder"
-    :disabled="disabled"
-    @input="handleInput"
-  />
+  <span class="input-wrapper" :class="{ 'input-wrapper--disabled': disabled }">
+    <span v-if="$slots['inline-leading-add-on']" class="input-add-on input-add-on--leading">
+      <slot name="inline-leading-add-on"></slot>
+    </span>
+
+    <input
+      :value="modelValue"
+      class="input"
+      :placeholder="placeholder"
+      :disabled="disabled"
+      @input="handleInput"
+    />
+
+    <slot name="trailing-add-on"></slot>
+  </span>
 </template>
 
 <style lang="scss" scoped>
-.input {
-  padding: 6px 12px;
+.input-wrapper {
+  position: relative;
+  box-sizing: border-box;
+  height: 36px;
   margin: 2px;
+  padding: 0px 12px;
+
+  display: inline-flex;
+  align-items: center;
 
   border-radius: 6px;
   border: 1px solid #d1d5db;
-
-  color: rgb(17 24 39);
-
-  outline: none;
   background-color: #fff;
+  overflow: hidden;
+  color: rgb(17 24 39);
 }
 
-.input::placeholder {
-  color: #a0aec0;
-  cursor: pointer;
-}
-
-.input:focus {
+.input-wrapper:focus-within {
   border: 1px solid rgb(79 70 229);
   outline: 1px solid rgb(79 70 229);
 }
 
-.input:disabled {
+.input-wrapper--disabled {
   background: rgb(249 250 251);
   cursor: not-allowed;
+
+  * {
+    cursor: not-allowed;
+  }
+}
+
+.input {
+  height: 100%;
+  font-family: inherit;
+  font-size: 14px;
+  line-height: 24px;
+  color: rgb(17 24 39);
+  border: none;
+  outline: none;
+  background-color: transparent;
+}
+
+.input::placeholder {
   color: #a0aec0;
+}
+
+.input:disabled {
+  color: #a0aec0;
+}
+
+.input-add-on {
+  user-select: none;
+  font-size: 14px;
+  color: rgb(107 114 128);
+}
+
+.input-add-on--leading {
+  margin-right: 4px;
 }
 </style>
