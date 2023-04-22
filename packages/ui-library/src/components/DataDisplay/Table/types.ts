f4485df1629f89c4de86dd49910bf572d40d87be
luohuidong <luohuidong01@126.com>
2023-04-22 19:44:06 +0800

    refactor: change table component comment

--- packages/ui-library/src/components/DataDisplay/Table/types.ts
+++ packages/ui-library/src/components/DataDisplay/Table/types.ts
@@ -5,36 +5,50 @@ export type Key = string | number;
 export type SortType = "ascending" | "descending";
 
 export interface Column {
+  /** Column key */
   key: string;
+  /** Column title */
   title: string;
-  /** When a column is fixed, it is necessary to provide the 'width' property */
+  /** Column width. When a column is fixed, it is necessary to provide the 'width' property */
   width?: number;
+  /** `left` or `right`. To set Column to fixed left or right of the table  */
   fixed?: "left" | "right";
+  /** To set Column to be Sortable */
   sortable?: boolean;
+  /** The column sort type. Array need to be one of the following:`ascending` or `descending`. */
   sortType?: SortType[];
-  /** ellipsis cell content via setting 'ellipsis' property. The 'ellipsis' property only valid the column not use the slot  */
+  /** If the text content in a cell exceeds the available space, it can be clipped and replaced with an ellipsis to indicate truncation  */
   ellipsis?: boolean;
 }
 
 export interface RootProps {
+  /** Row's unique key */
   rowKey: Key;
+  /** Table data */
   data: Record[];
   columns: Column[];
+  /** Enabled row can be expandable */
   rowExpand?: {
     expandCondition: (record: Record) => boolean;
     showExpandRowDefault?: boolean;
   };
+  /** Config of row sort  */
   sort?: {
     columnKey: Key;
     order: SortType;
   } | null;
+  /** Whether to show foot */
   showFoot?: boolean;
+  /** Config of row selection */
   selection?: {
     type: "multiple" | "single";
     disabledCondition?: (record: Record) => boolean;
   };
+  /** The set of selected row keys */
   selectedRowKeys?: Set<Key>;
+  /** Class name of table body row cell */
   tbodyRowClass?: string | ((record: Record) => string);
+  /** Class name of Table body row */
   tbodyCellClass?: string;
 }
 
