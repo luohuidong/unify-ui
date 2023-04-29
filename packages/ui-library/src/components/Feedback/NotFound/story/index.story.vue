bca09078186bc4617737cfa461d744209e6d151c
luohuidong <luohuidong01@126.com>
2023-04-29 14:58:19 +0800

    feat: add NotFound component

---
+++ packages/ui-library/src/components/Feedback/NotFound/story/index.story.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import EzNotFound from "../index.vue";
+</script>
+
+<template>
+  <Story title="Feedback/NotFound">
+    <div class="container">
+      <div>
+        <EzNotFound></EzNotFound>
+      </div>
+
+      <div>
+        <EzNotFound align="right"></EzNotFound>
+      </div>
+
+      <div>
+        <EzNotFound align="left"></EzNotFound>
+      </div>
+    </div>
+  </Story>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: grid;
+  grid-template-rows: repeat(3, 200px);
+  gap: 20px;
+}
+</style>
--- packages/ui-library/src/components/index.ts
+++ packages/ui-library/src/components/index.ts
@@ -20,3 +20,4 @@ export { default as EzEmpty } from "./DataDisplay/Empty/index.vue";
 // Feedback Components
 export { useAlertDialog, ConfirmDialog } from "./Feedback/Dialog";
 export { useMessage } from "./Feedback/Message";
+export { default as EzNotFound } from "./Feedback/NotFound/index.vue";
