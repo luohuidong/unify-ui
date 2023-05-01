e64742dd76dfb26b4b8a0fe7480a6bf89b550a64
luohuidong <luohuidong01@126.com>
2023-05-01 22:16:44 +0800

    feat: add Switch component

---
+++ packages/ui-library/src/components/DataEntry/Switch/story/index.story.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import EzSwitch from "../index.vue";
+
+const switchValue = ref(false);
+
+function handleChange(value: boolean) {
+  console.log("value", value);
+}
+</script>
+
+<template>
+  <Story title="Data Entry/Switch">
+    <Variant title="Basic Usage">
+      <EzSwitch v-model="switchValue" @change="handleChange"></EzSwitch>
+    </Variant>
+  </Story>
+</template>
+
+<style lang="scss" scoped></style>
