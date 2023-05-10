d0afec3aeda523931438b9183ee0949723243e29
luohuidong <luohuidong01@126.com>
2023-05-10 14:30:25 +0800

    refactor: change folder structures

--- packages/ui-library/src/components/index.ts
+++ packages/ui-library/src/components/index.ts
@@ -1,25 +1,30 @@
-export { default as UniButton } from "./Button/index.vue";
+// General
+export { default as UniButton } from "./general/Button/index.vue";
+
+// Data Entry Components
+export { UniCheckbox, UniCheckboxGroup } from "./data-entry/Checkbox";
+export { default as UniInput } from "./data-entry/Input/index.vue";
+export { default as UniFileDropZone } from "./data-entry/FileDropZone/index.vue";
+export { default as UniSwitch } from "./data-entry/Switch/index.vue";
+
+// Data Display Components
+export { default as UniPopup } from "./data-display/Popup/index.vue";
+export { default as UniToolTip } from "./data-display/Tooltip/index.vue";
+export { default as UniTable } from "./data-display/Table/index.vue";
+export { default as UniEmpty } from "./data-display/Empty/index.vue";
+
+// Feedback Components
+export { UniModal, useUniConfirmModal } from "./feedback/Modal";
+export { useMessage } from "./feedback/Message";
+export { default as UniNotFound } from "./feedback/NotFound/index.vue";
+
+// Navigation
 export {
   UniStandardDrawer,
   UniModalDrawer,
   UniNavigationList,
   UniNavigationListItem,
 } from "./NavigationDrawer";
-export { default as UniScrim } from "./UniScrim.vue";
 
-// Data Entry Components
-export { UniCheckbox, UniCheckboxGroup } from "./DataEntry/Checkbox";
-export { default as UniInput } from "./DataEntry/Input/index.vue";
-export { default as UniFileDropZone } from "./DataEntry/FileDropZone/index.vue";
-export { default as UniSwitch } from "./DataEntry/Switch/index.vue";
-
-// Data Display Components
-export { default as UniPopup } from "./DataDisplay/Popup/index.vue";
-export { default as UniToolTip } from "./DataDisplay/Tooltip/index.vue";
-export { default as UniTable } from "./DataDisplay/Table/index.vue";
-export { default as UniEmpty } from "./DataDisplay/Empty/index.vue";
-
-// Feedback Components
-export { UniModal, useUniConfirmModal } from "./Feedback/Modal";
-export { useMessage } from "./Feedback/Message";
-export { default as UniNotFound } from "./Feedback/NotFound/index.vue";
+// Others
+export { default as UniScrim } from "./others/Scrim/index.vue";
--- packages/ui-library/src/components/others/Scrim/index.vue
+++ packages/ui-library/src/components/others/Scrim/index.vue
@@ -1,3 +1,11 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "UniScrim",
+});
+</script>
+
 <script setup lang="ts">
 const emits = defineEmits(["click"]);
 
