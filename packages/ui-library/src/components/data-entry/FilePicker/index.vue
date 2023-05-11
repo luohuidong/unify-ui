fcf38a92bac558250413533707499676a2dfec83
luohuidong <luohuidong01@126.com>
2023-05-11 11:35:26 +0800

    feat: add FilePicker component

---
+++ packages/ui-library/src/components/data-entry/FilePicker/index.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FilePicker",
+});
+</script>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{
+  multiple?: boolean;
+}>();
+
+const emits = defineEmits<{
+  (e: "file-change", files: FileList): void;
+}>();
+
+const inputRef = ref();
+
+function handleClick() {
+  inputRef.value?.click();
+}
+
+function handleFileChange(event: Event) {
+  const input = event.target as HTMLInputElement;
+
+  const files = input.files;
+  if (files) emits("file-change", files);
+
+  input.value = "";
+}
+</script>
+
+<template>
+  <div class="file-picker" @click="handleClick">
+    <slot></slot>
+    <input
+      ref="inputRef"
+      class="file-picker__input"
+      type="file"
+      :multiple="multiple"
+      @change="handleFileChange"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.file-picker {
+  display: inline-block;
+  cursor: pointer;
+}
+
+.file-picker__input {
+  display: none;
+}
+</style>
