ec7492caa4aa3c330830f2df685649bf0a995716
luohuidong <luohuidong01@126.com>
2023-05-12 18:07:12 +0800

    build: update versions of dependencies

--- packages/ui-library/src/components/data-display/Table/story/useFetchData.ts
+++ packages/ui-library/src/components/data-display/Table/story/useFetchData.ts
@@ -10,9 +10,9 @@ type Posts = {
 
 export default function useFetchData() {
   const data: Posts = new Array(10).fill(0).map(() => ({
-    id: faker.datatype.uuid(),
+    id: faker.string.uuid(),
     title: faker.lorem.word(),
-    author: faker.name.fullName(),
+    author: faker.person.fullName(),
     post: faker.lorem.paragraph(),
     description: Math.random() > 0.7 ? faker.lorem.paragraph() : "",
   }));
