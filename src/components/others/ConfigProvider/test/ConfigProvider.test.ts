47296b9d6d0828ec1c32d07956ec72fc5d0a4af8
luohuidong <luohuidong01@126.com>
2023-06-01 22:38:34 +0800

    test: modify the test for the ConfigProvider component

--- src/components/others/ConfigProvider/test/ConfigProvider.test.ts
+++ src/components/others/ConfigProvider/test/ConfigProvider.test.ts
@@ -1,18 +1,38 @@
 import { mount } from "@vue/test-utils";
-import { UniConfigProvider, UniEmpty } from "unify-ui";
-import { h } from "vue";
+import { test, expect } from "vitest";
+import { h, defineComponent, inject } from "vue";
+import { UniConfigProvider } from "unify-ui";
+import { configKey } from "../utils";
 
-test("Internationalization", async () => {
-  const wrapper = mount(UniConfigProvider, {
-    props: {
-      locale: "en-US",
+test("locale and fallbackLocale", async () => {
+  const TestComponent = defineComponent({
+    setup() {
+      const config = inject(configKey);
+      return { config };
     },
+    template: `
+      <div>
+        <span id="locale">{{ config.locale }}</span>
+        <span id="fallbackLocale">{{ config.fallbackLocale }}</span>
+      </div>
+    `,
+  });
+
+  const wrapper = mount(UniConfigProvider, {
     slots: {
-      default: h(UniEmpty),
+      default: h(TestComponent),
     },
   });
-  expect(wrapper.html()).toContain("No Data");
 
-  await wrapper.setProps({ locale: "zh-CN" });
-  expect(wrapper.html()).toContain("暂无数据");
+  const locale = wrapper.find("#locale");
+  const fallbackLocale = wrapper.find("#fallbackLocale");
+
+  // default locale:en-Us, default fallbackLocale:en-US
+  expect(locale.text()).toContain("en-US");
+  expect(fallbackLocale.text()).toContain("en-US");
+
+  // locale: zh-CN, fallbackLocale: zh-CN
+  await wrapper.setProps({ locale: "zh-CN", fallbackLocale: "zh-CN" });
+  expect(locale.text()).toContain("zh-CN");
+  expect(fallbackLocale.text()).toContain("zh-CN");
 });
