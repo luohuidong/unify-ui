5e5e30ddcf061353e018c41015bf20cd32df7572
luohuidong <luohuidong01@126.com>
2023-06-05 14:31:03 +0800

    docs: add documentation explaning the feature of error status for the Input component

---
+++ docs/components/data-entry/input/ShowCaseStatusError.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { UniInput } from "unify-ui";
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      <UniInput status="error" class="input-cls"></UniInput>
+    </div>
+
+    <div>
+      <UniInput status="error" class="input-cls">
+        <template #inline-leading-add-on>
+          <span>http://</span>
+        </template>
+
+        <template #inline-trailing-add-on>
+          <span>.com</span>
+        </template>
+      </UniInput>
+    </div>
+
+    <div>
+      <UniInput status="error" class="input-cls">
+        <template #leading-add-on>
+          <span>http://</span>
+        </template>
+
+        <template #trailing-add-on>
+          <span>.com</span>
+        </template>
+      </UniInput>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container > div {
+  margin-bottom: 20px;
+}
+
+.input-cls {
+  width: 300px;
+}
+</style>
