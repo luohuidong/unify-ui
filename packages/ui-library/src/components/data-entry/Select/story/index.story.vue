82c89ecca4fc92777c8ac8f856b0b9c18d59c529
luohuidong <luohuidong01@126.com>
2023-06-22 16:00:58 +0800

    feat: add the Select component

---
+++ packages/ui-library/src/components/data-entry/Select/story/index.story.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import UniSelect from "../index.vue";
+
+const options = [
+  { label: "A", value: "a" },
+  { label: "B", value: "b" },
+  { label: "C", value: "c" },
+  { label: "D", value: "d" },
+  { label: "E", value: "e" },
+  { label: "F", value: "f" },
+];
+
+const value = ref();
+</script>
+
+<template>
+  <Story title="Data Entry/Select">
+    <div class="container">
+      <UniSelect v-model="value" :options="options"> </UniSelect>
+    </div>
+  </Story>
+</template>
+
+<style scoped>
+.container {
+  width: 500px;
+  height: 500px;
+  padding: 20px;
+}
+</style>
