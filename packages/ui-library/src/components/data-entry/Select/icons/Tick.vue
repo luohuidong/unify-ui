abc7446a85dcb95044f86436d901cbcc5662a121
luohuidong <luohuidong01@126.com>
2023-06-23 01:53:21 +0800

    refactor: revise the css style of the Select component

---
+++ packages/ui-library/src/components/data-entry/Select/icons/Tick.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <svg
+    width="32px"
+    height="32px"
+    viewBox="0 0 32 32"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+  >
+    <g id="tick" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <polygon
+        id="path"
+        fill="currentColor"
+        points="25.1840272 8.34314575 12.4667118 21.0604612 5.39527351 13.9897638 3.27395317 16.1110842 10.3453914 23.1817816 10.3347848 23.1923882 12.4561052 25.3137085 27.3053476 10.4644661"
+      ></polygon>
+    </g>
+  </svg>
+</template>
+
+<style lang="scss" module></style>
