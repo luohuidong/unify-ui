edcafe7742955083da69671d04a4c635a0aa5ed4
luohuidong <luohuidong01@126.com>
2023-06-22 20:25:54 +0800

    feat: add the Tag component

---
+++ .changeset/grumpy-spiders-act.md
@@ -0,0 +1,5 @@
+---
+"unify-ui": minor
+---
+
+Add the tag component
---
+++ packages/ui-library/src/components/data-display/Tag/index.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "UniTag",
+});
+</script>
+
+<script setup lang="ts"></script>
+
+<template>
+  <span :class="$style.tag">
+    <slot></slot>
+  </span>
+</template>
+
+<style lang="scss" module>
+@use "@/styles/color";
+
+.tag {
+  padding: 4px 8px;
+  font-size: 12px;
+  color: color.$gray-500;
+  background: color.$gray-50;
+  border: 1px solid color.$gray-200;
+  border-radius: 6px;
+}
+</style>
