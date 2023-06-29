87253cbff68f277b1eb02d2e43fb6f30adb80d6a
luohuidong <luohuidong01@126.com>
2023-06-29 23:55:07 +0800

    docs: update the story of the Button component

---
+++ packages/ui-library/src/components/general/Button/Button.stories.ts
@@ -0,0 +1,83 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import { action } from "@storybook/addon-actions";
+
+import UniButton from "./index.vue";
+
+const meta = {
+  title: "General/Button",
+  component: UniButton,
+  tags: ["autodocs"],
+  argTypes: {
+    type: { control: "radio", options: ["primary", "secondary", "soft"] },
+    size: { control: "radio", options: ["xs", "s", "m", "l", "xl"] },
+    htmlType: { control: "radio", options: ["button", "submit", "reset"] },
+    onClick: {},
+  },
+  args: {
+    type: "primary",
+    size: "m",
+    disabled: false,
+    rounded: false,
+    htmlType: "button",
+  },
+  excludeStories: /.*Data$/,
+} satisfies Meta<typeof UniButton>;
+
+export default meta;
+type Story = StoryObj<typeof UniButton>;
+
+export const actionsData = {
+  onClick: action("click"),
+};
+
+export const PrimaryButton: Story = {
+  render: (args) => ({
+    components: { UniButton },
+    setup: () => ({ args }),
+    template: '<UniButton v-bind="args">UniButton</UniButton>',
+  }),
+};
+
+export const SecondaryButton: Story = {
+  render: (args) => ({
+    components: { UniButton },
+    setup: () => ({ args }),
+    template: '<UniButton v-bind="args">UniButton</UniButton>',
+  }),
+  args: {
+    type: "secondary",
+  },
+};
+
+export const SoftButton: Story = {
+  render: (args) => ({
+    components: { UniButton },
+    setup: () => ({ args }),
+    template: '<UniButton v-bind="args">UniButton</UniButton>',
+  }),
+  args: {
+    type: "soft",
+  },
+};
+
+export const Disabled: Story = {
+  render: (args) => ({
+    components: { UniButton },
+    setup: () => ({ args }),
+    template: '<UniButton v-bind="args">UniButton</UniButton>',
+  }),
+  args: {
+    disabled: true,
+  },
+};
+
+export const BorderRounded: Story = {
+  render: (args) => ({
+    components: { UniButton },
+    setup: () => ({ args }),
+    template: '<UniButton v-bind="args">UniButton</UniButton>',
+  }),
+  args: {
+    rounded: true,
+  },
+};
