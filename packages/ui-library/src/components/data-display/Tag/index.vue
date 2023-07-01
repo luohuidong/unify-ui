503479fc28ab0830b9af4ade856681c2ef82ee34
luohuidong <luohuidong01@126.com>
2023-07-01 18:40:05 +0800

    feat: create a set of predefined colorful tag styles for various usage scenarios

--- packages/ui-library/src/components/data-display/Tag/index.vue
+++ packages/ui-library/src/components/data-display/Tag/index.vue
@@ -7,13 +7,33 @@ export default defineComponent({
 </script>
 
 <script setup lang="ts">
+withDefaults(
+  defineProps<{
+    type: "default" | "success" | "info" | "warning" | "error";
+  }>(),
+  {
+    type: "default",
+  }
+);
+
 defineSlots<{
   default(props: {}): any;
 }>();
 </script>
 
 <template>
-  <span :class="$style.tag">
+  <span
+    :class="[
+      $style.tag,
+      $style['tag--type-default'],
+      {
+        [$style['tag--type-info']]: type === 'info',
+        [$style['tag--type-success']]: type === 'success',
+        [$style['tag--type-warning']]: type === 'warning',
+        [$style['tag--type-error']]: type === 'error',
+      },
+    ]"
+  >
     <slot></slot>
   </span>
 </template>
@@ -22,11 +42,41 @@ defineSlots<{
 @use "@/styles/color";
 
 .tag {
+  margin-right: 6px;
   padding: 4px 8px;
   font-size: 12px;
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 6px;
+}
+
+.tag--type-default {
   color: color.$gray-500;
   background: color.$gray-50;
-  border: 1px solid color.$gray-200;
-  border-radius: 6px;
+  border-color: color.$gray-200;
+}
+
+.tag--type-info {
+  color: color.$info-font;
+  background: color.$info-background;
+  border-color: color.$info-border;
+}
+
+.tag--type-success {
+  color: color.$success-font;
+  background: color.$success-background;
+  border-color: color.$success-border;
+}
+
+.tag--type-error {
+  color: color.$error-font;
+  background: color.$error-background;
+  border-color: color.$error-border;
+}
+
+.tag--type-warning {
+  color: color.$warning-font;
+  background: color.$warning-background;
+  border-color: color.$warning-border;
 }
 </style>
