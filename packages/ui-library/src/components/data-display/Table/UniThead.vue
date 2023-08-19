040d00d6c8f231bcc9658f63b4964d5491fd9bda
luohuidong <luohuidong01@126.com>
2023-08-19 15:57:48 +0800

    refactor: optimize the implementation of the table header

--- packages/ui-library/src/components/data-display/Table/UniThead.vue
+++ packages/ui-library/src/components/data-display/Table/UniThead.vue
@@ -1,79 +1,31 @@
 <script setup lang="ts">
-import { useInject, useSortEmit } from "./composables";
-import commonStyle from "./commonStyle.module.scss";
-import { ColumnData } from "./types";
+import { useInject } from "./composables";
 
 import UniTheadSelectionCell from "./UniTheadSelectionCell.vue";
 import UniTheadSort from "./UniTheadSort.vue";
 import UniTheadExpandColumnCell from "./UniTheadExpandColumnCell.vue";
+import UniTheadNormalCell from "./UniTheadNormalCell.vue";
 
 const {
   columnsInfo: { columnsData },
-  showShadow,
   tableProps,
   rootSlotKeys,
   tableState,
 } = useInject();
-const { handleSortEmit } = useSortEmit();
-
-function handleSort(column: ColumnData) {
-  if (!column.sortable) return;
-
-  const currentSort = tableProps.sort;
-  const sortTypes = column.sortType || ["ascending", "descending"];
-
-  // The first specified sort type in `sortTypes` will be used to sort the current column if the table is not sorted.
-  if (!currentSort) {
-    handleSortEmit({ columnKey: column.key, order: sortTypes[0] });
-    return;
-  }
-
-  // The first specified sort type in `sortTypes` will be used to sort the current column if the current column is not sorted.
-  if (currentSort.columnKey !== column.key) {
-    handleSortEmit({ columnKey: column.key, order: sortTypes[0] });
-    return;
-  }
-
-  // change current column sort order
-  const index = sortTypes.findIndex((sortType) => sortType === currentSort.order);
-  if (index === sortTypes.length - 1) {
-    handleSortEmit(null);
-  } else {
-    handleSortEmit({ columnKey: column.key, order: sortTypes[index + 1] });
-  }
-}
 </script>
 
 <template>
   <thead>
     <tr>
       <template v-if="tableState.showExpandToggleCell">
-        <UniTheadExpandColumnCell :class="$style['header__cell']"></UniTheadExpandColumnCell>
+        <UniTheadExpandColumnCell></UniTheadExpandColumnCell>
       </template>
 
       <template v-if="tableProps.selection?.type">
-        <UniTheadSelectionCell :class="$style['header__cell']"></UniTheadSelectionCell>
+        <UniTheadSelectionCell></UniTheadSelectionCell>
       </template>
 
-      <th
-        v-for="col in columnsData"
-        :key="col.key"
-        scope="col"
-        :style="{
-          left: col.leftOffset && `${col.leftOffset}px`,
-          right: col.rightOffset && `${col.rightOffset}px`,
-          zIndex: col.fixed ? 3 : 2,
-        }"
-        :class="[
-          $style['header__cell'],
-          {
-            [commonStyle['cell--shadow-right']]: col.leftLastFixedColumn && showShadow.showLeftFixedColumnShadow,
-            [commonStyle['cell--shadow-left']]: col.rightFirstFixedColumn && showShadow.showRightFixedColumnShadow,
-            [$style['head__cell--sortable']]: col.sortable,
-          },
-        ]"
-        @click.stop="handleSort(col)"
-      >
+      <UniTheadNormalCell v-for="col in columnsData" :key="col.key" :col="col">
         <UniTheadSort v-if="col.sortable" :column-data="col"></UniTheadSort>
 
         <template v-if="rootSlotKeys.has(`header-${col.key}`)">
@@ -82,31 +34,7 @@ function handleSort(column: ColumnData) {
         <template v-else>
           <span>{{ col.title }}</span>
         </template>
-      </th>
+      </UniTheadNormalCell>
     </tr>
   </thead>
 </template>
-
-<style lang="scss" module>
-.header__cell {
-  box-sizing: border-box;
-  position: sticky;
-  top: 0;
-  padding: 14px 12px;
-
-  word-wrap: break-word;
-  text-align: left;
-  font-size: 14px;
-  line-height: 20px;
-  font-weight: 600;
-  color: rgb(17 24 39);
-  background: white;
-  border-bottom: 1px solid rgb(226 232 240);
-  user-select: none;
-}
-
-.head__cell--sortable {
-  cursor: pointer;
-  padding-left: 24px;
-}
-</style>
