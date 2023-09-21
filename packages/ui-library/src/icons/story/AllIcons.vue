2e6bcdaead3536a53ca9ebf7a5f48e8c26917a04
luohuidong <luohuidong01@126.com>
2023-09-21 23:34:24 +0800

    refactor: add the Tick icon

--- packages/ui-library/src/icons/story/AllIcons.vue
+++ packages/ui-library/src/icons/story/AllIcons.vue
@@ -4,30 +4,35 @@ import ArrowRight from '../assets/ArrowRight.vue'
 import Close from '../assets/Close.vue'
 import DoubleArrowLeft from '../assets/DoubleArrowLeft.vue'
 import DoubleArrowRight from '../assets/DoubleArrowRight.vue'
+import Tick from '../assets/Tick.vue'
 
 </script>
 
 <template>
 <div class="container">
     <div class="icon-wrapper">
-    <div><ArrowLeft /></div>
+    <div><ArrowLeft class="icon" /></div>
     <div>ArrowLeft</div>
   </div>
     <div class="icon-wrapper">
-    <div><ArrowRight /></div>
+    <div><ArrowRight class="icon" /></div>
     <div>ArrowRight</div>
   </div>
     <div class="icon-wrapper">
-    <div><Close /></div>
+    <div><Close class="icon" /></div>
     <div>Close</div>
   </div>
     <div class="icon-wrapper">
-    <div><DoubleArrowLeft /></div>
+    <div><DoubleArrowLeft class="icon" /></div>
     <div>DoubleArrowLeft</div>
   </div>
     <div class="icon-wrapper">
-    <div><DoubleArrowRight /></div>
+    <div><DoubleArrowRight class="icon" /></div>
     <div>DoubleArrowRight</div>
+  </div>
+    <div class="icon-wrapper">
+    <div><Tick class="icon" /></div>
+    <div>Tick</div>
   </div>
   
 </div>
@@ -45,5 +50,10 @@ import DoubleArrowRight from '../assets/DoubleArrowRight.vue'
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+
+.icon {
+  width: 20px;
+  height: 20px;
 }
 </style>
