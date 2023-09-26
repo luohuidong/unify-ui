3bbf1e8c7791a1711d30447deaca1cc5d8a26e63
luohuidong <luohuidong01@126.com>
2023-09-27 00:10:06 +0800

    docs: update documentation for the Select component

--- packages/docs/components/data-entry/select/ShowCaseBasicUsage.vue
+++ packages/docs/components/data-entry/select/ShowCaseBasicUsage.vue
@@ -1,27 +1,30 @@
 <script setup lang="ts">
-import { UniSelect } from "unify-ui";
+import { UniSelect, UniSelectOption } from "unify-ui";
 import { ref } from "vue";
 
 const value = ref<string>();
 
 const options = [
-  {
-    label: "Option 1",
-    value: "option-1",
-  },
-  {
-    label: "Option 2",
-    value: "option-2",
-  },
-  {
-    label: "Option 3",
-    value: "option-3",
-  },
+  { label: "A", value: "a" },
+  { label: "B", value: "b" },
+  { label: "C", value: "c" },
+  { label: "D", value: "d" },
+  { label: "E", value: "e" },
+  { label: "F", value: "f" },
 ];
 </script>
 
 <template>
-  <UniSelect v-model="value" :options="options" />
+  <div>
+    <UniSelect v-model="value">
+      <UniSelectOption
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      ></UniSelectOption>
+    </UniSelect>
+  </div>
 </template>
 
 <style lang="scss" module></style>
