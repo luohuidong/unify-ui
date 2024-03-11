1eb090047ed862b55957c2735aa861db0367a4a4
luohuidong <luohuidong01@126.com>
2024-03-11 11:15:02 +0800

    build: modify build script

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -3,6 +3,10 @@
 ./scripts/build.sh
 
 pnpm \
+    --filter "@unify-ui/vite-plugin-vue-docs" \
+    build
+
+pnpm \
     --filter "@unify-ui/vue-docs" \
     build
 
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 pnpm \
-    --filter "@unify-ui/vite-plugin-vue-docs" \
     --filter "@unify-ui/web-components" \
     build
 
