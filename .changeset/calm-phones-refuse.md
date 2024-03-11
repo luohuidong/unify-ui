6e669d6a66cfbe621e7c7b5339dd7d3ccffab1f8
luohuidong <luohuidong01@126.com>
2024-03-12 00:34:01 +0800

    feat: add the u-pagination component

---
+++ .changeset/calm-phones-refuse.md
@@ -0,0 +1,5 @@
+---
+"@unify-ui/web-components": minor
+---
+
+add the u-pagination component
---
+++ packages/web-components/src/pagination/story/basic-usage.ts
@@ -0,0 +1,33 @@
+import { LitElement, html } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+
+import '../index.js';
+
+@customElement('show-case-pagination-basic-usage')
+class ShowCaseBasicUsage extends LitElement {
+  @state()
+  total = 500;
+
+  @state()
+  pageSize = 20;
+
+  @state()
+  current = 1;
+
+  handleChange(e: CustomEvent<{ current: number; pageSize: number }>) {
+    this.current = e.detail.current;
+    this.pageSize = this.pageSize;
+  }
+
+  render() {
+    return html`
+      <u-pagination
+        .total=${this.total}
+        .pageSize=${this.pageSize}
+        .current=${this.current}
+        @change=${this.handleChange}
+      >
+      </u-pagination>
+    `;
+  }
+}
