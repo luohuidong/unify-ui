e49e6c9931e9c06081f3686d3e89a12a896072e5
luohuidong <luohuidong01@126.com>
2024-03-13 18:41:59 +0800

    build: upgrade storybook

--- packages/web-components/.storybook/preview.ts
+++ packages/web-components/.storybook/preview.ts
@@ -1,9 +1,8 @@
-import type { Preview } from '@storybook/web-components'
-import '../src/styles/base.css'
+import type { Preview } from "@storybook/web-components";
+import "../src/styles/base.css";
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
@@ -11,6 +10,6 @@ const preview: Preview = {
       },
     },
   },
-}
+};
 
-export default preview
+export default preview;
