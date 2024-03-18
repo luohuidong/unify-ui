f2931581a13c9ac8d6c763f66c6aae0b376b8c5c
luohuidong <luohuidong01@126.com>
2024-03-19 00:08:13 +0800

    docs: add documentation for the pagination component

---
+++ packages/vue-docs/docs/components/data-display/pagination/ShowCaseBaseUsage.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { UniPagination } from "unify-ui";
+
+const total = ref(100);
+const pageSize = ref(10);
+const current = ref(1);
+
+function handleChange(params: { current: number; pageSize: number }) {
+  console.log(params);
+  current.value = params.current;
+  pageSize.value = params.pageSize;
+}
+</script>
+
+<template>
+  <div>total: {{ total }}</div>
+  <div>pageSize: {{ pageSize }}</div>
+  <div>current: {{ current }}</div>
+
+  <UniPagination
+    :total="total"
+    :page-size="pageSize"
+    :current="current"
+    @chagne="handleChange"
+  ></UniPagination>
+</template>
